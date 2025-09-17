c692385372913dc088a60881fdc8f0b9004a8635
Francesco Valentini <Francesco.valentini930@gmail.com>
2025-09-17 15:20:41 +0200

    Added PKCS12Keystore

---
+++ src/it/hackerinside/etk/core/keystore/PKCS12Keystore.java
@@ -0,0 +1,71 @@
+package it.hackerinside.etk.core.keystore;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.security.KeyStore;
+import java.util.Objects;
+
+import it.hackerinside.etk.core.Models.SupportedKeystores;
+
+/**
+ * Concrete implementation of AbstractKeystore for PKCS12 format keystores.
+ * This class provides functionality to load and save PKCS12 keystores from/to files.
+ * 
+ * @author Francesco Valentini
+ */
+class PKCS12Keystore extends AbstractKeystore {
+	
+    /**
+     * The file where the PKCS12 keystore is stored.
+     */
+    private final File file;
+    
+    /**
+     * The password used to protect the keystore.
+     * (keystore master password)
+     */
+    private final char[] password;
+
+    /**
+     * Constructs a new PKCS12Keystore with the specified file path and password.
+     *
+     * @param filePath the path to the PKCS12 keystore file
+     * @param password the password used to protect the keystore
+     * @throws NullPointerException if filePath or password is null
+     */
+    public PKCS12Keystore(String filePath, char[] password) {
+    	super(SupportedKeystores.PKCS12);
+        this.file = new File(filePath);
+        this.password = password;
+    }
+    
+    /**
+     * Constructs a new PKCS12Keystore with the specified File object and password.
+     *
+     * @param file the PKCS12 keystore file
+     * @param password the password used to protect the keystore
+     * @throws NullPointerException if file or password is null
+     */
+    public PKCS12Keystore(File file, char[] password) {
+    	super(SupportedKeystores.PKCS12);
+    	Objects.requireNonNull(file);
+        this.file = file;
+        this.password = password;
+    }
+
+    @Override
+    public void load() throws Exception {
+        keyStore = KeyStore.getInstance("PKCS12");
+        try (FileInputStream fis = new FileInputStream(file)) {
+            keyStore.load(fis, password);
+        }
+    }
+
+    @Override
+    public void save() throws Exception {
+        try (FileOutputStream fos = new FileOutputStream(file)) {
+            keyStore.store(fos, password);
+        }
+    }
+}
