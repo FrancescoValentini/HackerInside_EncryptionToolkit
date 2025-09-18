7b8c937fa419f643db4d222d939723408f4d2c79
Francesco Valentini <Francesco.valentini930@gmail.com>
2025-09-18 16:04:01 +0200

    Improved testing system

--- src/it/hackerinside/etk/TEST/EncryptTest.java
+++ src/it/hackerinside/etk/TEST/EncryptTest.java
@@ -3,15 +3,11 @@ package it.hackerinside.etk.TEST;
 import java.io.File;
 import java.security.Security;
 import java.security.cert.X509Certificate;
-import java.util.Enumeration;
-
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 import it.hackerinside.etk.core.Encryption.CMSEncryptor;
 import it.hackerinside.etk.core.Models.EncodingOption;
 import it.hackerinside.etk.core.Models.SymmetricAlgorithms;
-import it.hackerinside.etk.core.keystore.AbstractKeystore;
-import it.hackerinside.etk.core.keystore.PKCS12Keystore;
 
 public class EncryptTest {
 	static {
@@ -19,41 +15,15 @@ public class EncryptTest {
 	}
 	public static void main(String[] args) throws Exception {
 
-		File rsaKeystore = new File("RSATEST.pfx");
-		File eccKeystore = new File("ECCTEST.pfx");
-		
+		KeystoreTest ks = new KeystoreTest();
 		File toEncrypt = new File("file.test");
-		
 		String rsaAlias = "rsatest", eccAlias = "ecctest";
 		
-		char[] pwd = "123".toCharArray();
+		// Load certificates
+		X509Certificate rsaCert = ks.pkcs12_rsa.getCertificate(rsaAlias);
+		X509Certificate eccCert = ks.pkcs12_ecc.getCertificate(eccAlias);
 		
-		// 1) Opens the keystore
-		AbstractKeystore pkcs12_rsa = new PKCS12Keystore(rsaKeystore,pwd); 
-		AbstractKeystore pkcs12_ecc = new PKCS12Keystore(eccKeystore,pwd); 
-		pkcs12_rsa.load();
-		pkcs12_ecc.load();
-		
-		// 2) Print certificates informations
-		Enumeration<String> rsaCerts = pkcs12_rsa.listAliases();
-		Enumeration<String> eccCerts = pkcs12_ecc.listAliases();
-		
-		System.out.println("RSA ALIASES");
-		rsaCerts.asIterator().forEachRemaining(id -> System.out.println("  - " + id));
-		
-		System.out.println("\nECC ALIASES");
-		eccCerts.asIterator().forEachRemaining(id -> System.out.println("  - " + id));
-		
-		X509Certificate rsaCert = pkcs12_rsa.getCertificate(rsaAlias);
-		X509Certificate eccCert = pkcs12_ecc.getCertificate(eccAlias);
-		System.out.println("==== RSA CERTIFICATE ====");
-		System.out.println(rsaCert);
-		
-		System.out.println("==== ECC CERTIFICATE ====");
-		System.out.println(eccCert);
-		
-		
-		// 3) Encrypt 
+		// Encrypt 
 		System.out.println("RSA ENCRYPT");
 		CMSEncryptor rsaEncryptor = new CMSEncryptor(rsaCert, SymmetricAlgorithms.AES_256_CBC, EncodingOption.ENCODING_DER);
 		rsaEncryptor.encrypt(toEncrypt, new File("enc_rsa.test"));
