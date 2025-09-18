4108b9e084e5778c7a270fb69f4a122879ddbd95
Francesco Valentini <Francesco.valentini930@gmail.com>
2025-09-18 21:06:36 +0200

    Added NISTObjectIdentifiers to hashing algorithms

--- src/it/hackerinside/etk/core/Models/HashAlgorithm.java
+++ src/it/hackerinside/etk/core/Models/HashAlgorithm.java
@@ -1,5 +1,8 @@
 package it.hackerinside.etk.core.Models;
 
+import org.bouncycastle.asn1.ASN1ObjectIdentifier;
+import org.bouncycastle.asn1.nist.NISTObjectIdentifiers;
+
 /**
  * Represents supported cryptographic hash algorithms.
  * This enum provides commonly used Secure Hash Algorithm (SHA) variants.
@@ -10,22 +13,28 @@ public enum HashAlgorithm {
     /**
      * SHA-256 hash algorithm (produces 256-bit/32-byte hash values).
      */
-    SHA256("sha256"),
+    SHA256("sha256",NISTObjectIdentifiers.id_sha256),
     
     /**
      * SHA-384 hash algorithm (produces 384-bit/48-byte hash values).
      */
-    SHA384("sha384"),
+    SHA384("sha384",NISTObjectIdentifiers.id_sha384),
     
     /**
      * SHA-512 hash algorithm (produces 512-bit/64-byte hash values).
      */
-    SHA512("sha512");
+    SHA512("sha512",NISTObjectIdentifiers.id_sha384);
     
     private final String algorithm;
     
-    private HashAlgorithm(String algorithm) {
+    /**
+     * The ASN.1 object identifier for the algorithm.
+     */
+    private final ASN1ObjectIdentifier asn1;
+    
+    private HashAlgorithm(String algorithm,ASN1ObjectIdentifier asn1) {
         this.algorithm = algorithm;
+        this.asn1 = asn1;
     }
     
     /**
@@ -52,5 +61,15 @@ public enum HashAlgorithm {
     @Override
     public String toString() {
         return algorithm;
+    }
+    
+    
+    /**
+     * Returns the ASN.1 object identifier for the algorithm.
+     *
+     * @return the ASN.1 object identifier
+     */
+    public ASN1ObjectIdentifier getASN1() {
+    	return this.asn1;
     }
 }
--- src/module-info.java
+++ src/module-info.java
@@ -8,4 +8,5 @@ module HackerInside_EncryptionToolkit {
 	requires java.desktop;
 	requires org.bouncycastle.pkix;
 	requires org.bouncycastle.provider;
+	requires org.bouncycastle.util;
 }
