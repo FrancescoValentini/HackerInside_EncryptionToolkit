b140757fce1a665622031bdc5ce93a087ce42888
Francesco Valentini <francesco.valentini930@gmail.com>
2025-12-28 11:32:42 +0100

    Updated software version

--- src/it/hackerinside/etk/GUI/ETKContext.java
+++ src/it/hackerinside/etk/GUI/ETKContext.java
@@ -22,7 +22,7 @@ import it.hackerinside.etk.core.keystore.PKCS12Keystore;
  */
 public class ETKContext {
 	
-	public static final String ETK_VERSION = "1.0.5";
+	public static final String ETK_VERSION = "1.0.6";
 	
     /**
      * Singleton instance of ETKContext.
