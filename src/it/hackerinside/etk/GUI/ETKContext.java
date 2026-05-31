a93a83964a6e5f3faf578c0c9fe25d89c6ab2ff3
Francesco Valentini <francesco.valentini930@gmail.com>
2026-05-31 19:20:58 +0200

    Updated version number and fixed public key export bug

--- src/it/hackerinside/etk/GUI/ETKContext.java
+++ src/it/hackerinside/etk/GUI/ETKContext.java
@@ -28,7 +28,7 @@ import it.hackerinside.etk.core.keystore.PKCS12Keystore;
  */
 public class ETKContext {
 	
-	public static final String ETK_VERSION = "1.0.14";
+	public static final String ETK_VERSION = "1.0.15";
 	
     /**
      * Singleton instance of ETKContext.
